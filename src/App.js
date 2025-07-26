24c8644143e12255059c823dc3bb49c950efdc25
edmond <644568491@qq.com>
2025-07-27 00:30:15 +0800

    add react route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import Counter from './counter/index.js';
 import TreeQuiz from './tree-quiz/index.js';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>React Scenario Examples</h1>
+      <nav>
+        <ul>
+          <li><Link to="/timer">Timer</Link></li>
+          <li><Link to="/favorite-gundam">Favorite Gundam</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 const App = () => {
     return (
         <div className="App">
-            <h2 className="Case">Timer</h2>
-            <Counter />
-            <h2 className="Case">Favorite Gundam</h2>
-            <TreeQuiz />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/timer" element={<Counter />} />
+                <Route path="/favorite-gundam" element={<TreeQuiz />} />
+            </Routes>
         </div>
     );
 };
