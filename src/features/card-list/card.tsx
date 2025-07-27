025a516102d8959722760b69007c30812db43358
edmond <644568491@qq.com>
2025-07-27 16:44:02 +0800

    add time convertor

--- src/features/card-list/card.tsx
+++ src/features/card-list/card.tsx
@@ -7,8 +7,30 @@ interface CardProps {
     error: string | null;
 }
 
+const format = (input: number) => {
+    const date = new Date(input);
+
+    const year = `${date.getFullYear()}`.padStart(4, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const day = `${date.getDate()}`.padStart(2, '0');
+
+    const hours24 = date.getHours();
+    let amPm = 'AM';
+    if (hours24 >= 12) {
+        amPm = 'PM';
+    }
+    const hours12 = hours24 % 12 || 12;
+
+    const minutes = `${date.getMinutes()}`.padStart(2, '0');
+    const seconds = `${date.getSeconds()}`.padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours12}:${minutes}:${seconds} ${amPm}`;
+};
+
 const Card: React.FC<CardProps> = ({ post, error }) => {
     console.log(`render card #${post.id}`);
+
+    const timestamp = Date.now();
 
     return (
         <div className="card" key={post.id}>
@@ -19,8 +41,11 @@ const Card: React.FC<CardProps> = ({ post, error }) => {
             )}
             {!error && (
                 <>
-                    <h4>
+                    <h4 className="card-title">
                         #{post.id} {post.title}
+                        <span className="post-fetch-datetime-text">
+                            {format(timestamp)}
+                        </span>
                     </h4>
                     <p>{post.body}</p>
                 </>
