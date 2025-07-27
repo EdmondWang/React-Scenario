3a5090a2fe3e4e4f8abf728e2a3729084ec2fccf
edmond <644568491@qq.com>
2025-07-27 22:49:06 +0800

    add description

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,33 @@ function Home() {
                 <ul>
                     <li>
                         <Link to="/timer">Timer</Link>
+                        <span className="description">
+                            Closure trap of useEffect
+                        </span>
                     </li>
                     <li>
                         <Link to="/favorite-gundam">Favorite Gundam</Link>
+                        <span className="description">
+                            Recursively render and update
+                        </span>
                     </li>
                     <li>
                         <Link to="/todo-list">Todo List</Link>
+                        <span className="description">
+                            React.useCallBack and React.memo
+                        </span>
                     </li>
                     <li>
                         <Link to="/progress-bar">Progress Bar</Link>
+                        <span className="description">
+                            React.useCallBack and React.memo
+                        </span>
                     </li>
                     <li>
                         <Link to="/card-list">Card List</Link>
+                        <span className="description">
+                            Initial data loading and subsequent pagination
+                        </span>
                     </li>
                 </ul>
             </nav>
