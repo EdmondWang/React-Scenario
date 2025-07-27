48f931f2eef27f390d2e214d08599d5640ff44db
edmond <644568491@qq.com>
2025-07-27 17:30:03 +0800

    display post on page load

--- src/features/card-list/index.tsx
+++ src/features/card-list/index.tsx
@@ -13,9 +13,11 @@ const CardListFeature = () => {
     const [idIndex, setIdIndex] = useState<number>(0);
     const [postList, setPostList] = useState<PostState[]>([]);
 
-    const clickAddCards = async () => {
-        const newIdIndex = idIndex + 6;
-        const reqIdList = idList.slice(idIndex, newIdIndex);
+    const fetchPosts = async (
+        reqIdList: string[],
+        { flush }: { flush: boolean } = { flush: false }
+    ) => {
+        console.log('fetchPosts', reqIdList);
         const newCardPromises = reqIdList.map((id) => {
             return fetch(
                 `https://jsonplaceholder.typicode.com/posts/${id}`
@@ -43,14 +45,29 @@ const CardListFeature = () => {
             }
         });
 
-        setPostList((prev) => [...prev, ...newPosts]);
+        setPostList((prev) => (flush ? newPosts : [...prev, ...newPosts]));
+    };
+
+    const fetchPaginatedPosts = async () => {
+        const newIdIndex = idIndex + 6;
+        const reqIdList = idList.slice(idIndex, newIdIndex);
+        await fetchPosts(reqIdList);
         setIdIndex(newIdIndex);
+    };
+
+    const clickAddCards = async () => {
+        await fetchPaginatedPosts();
     };
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts').then((res) => {
-            res.json().then((data) => {
-                setIdList(data.map((d: { id: string }) => d.id));
+            res.json().then(async (data) => {
+                const newIdList = data.map((d: { id: string }) => d.id);
+                setIdList(newIdList);
+                const newIdIndex = idIndex + 6;
+                const reqIdList = newIdList.slice(idIndex, newIdIndex);
+                await fetchPosts(reqIdList, { flush: true });
+                setIdIndex(newIdIndex);
             });
         });
     }, []);
